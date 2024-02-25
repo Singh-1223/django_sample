02aa66bcf688ffb5e94aeff7e9c57122250f2021
Lovepreet singh <126049926+Singh-1223@users.noreply.github.com>
2024-02-25 16:43:05 +0530

    Update views.py

--- samp/views.py
+++ samp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 def health(request):
-    return "Sample app is up and running"
+    return JsonResponse(data={"message": "app is up and running"})
